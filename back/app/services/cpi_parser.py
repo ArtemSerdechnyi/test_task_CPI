e83c09072072849ec24258b20a1bbc92f8de7992
Kyrylo Lipovok <k.lipovok@gmail.com>
2026-02-03 14:52:37 +0200

    back: reorganize logic for parser

---
+++ back/app/services/cpi_parser.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+class GermanyHistoricalCpiParser:
+    def __init__(self):
+        self.url = "https://www.rateinflation.com/consumer-price-index/germany-historical-cpi/"
+        self.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+
+    months = {
+        "jan": "01",
+        "feb": "02",
+        "mar": "03",
+        "apr": "04",
+        "may": "05",
+        "jun": "06",
+        "jul": "07",
+        "aug": "08",
+        "sep": "09",
+        "oct": "10",
+        "nov": "11",
+        "dec": "12"
+    }
+
+    def parse_into_csv(self) -> dict[str, str]:
+        response = requests.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        table = soup.find("table")
+        if not table:
+            return {"detail": "Data not found."}
+
+        headers_row = [th.text.strip() for th in table.find("thead").find_all("th")]
+
+        rows = []
+        for tr in table.find("tbody").find_all("tr"):
+            cells = tr.find_all("td")
+            if not cells:
+                continue
+
+            year = cells[0].text.strip()
+
+            for i in range(1, 13):
+                month_name = self.months[headers_row[i]]
+                value = cells[i].text.strip()
+
+                if value:
+                    rows.append({
+                        "Date": f"{year}-{month_name}",
+                        "CPI": value
+                    })
+
+        df = pd.DataFrame(rows)
+
+        df.to_csv("germany_cpi_historical.csv", index=False, encoding="utf-8")
+        return {"detail": "Data parsed successfully."}
