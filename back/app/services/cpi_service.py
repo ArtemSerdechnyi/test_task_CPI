35dcf171ffe615e335999752d7cea1033808e17c
Artem Serdechnyi <artem.serdechnyy@gmail.com>
2026-02-03 17:01:27 +0200

    back: add cpi service

--- back/app/services/cpi_service.py
+++ back/app/services/cpi_service.py
@@ -1,5 +1,11 @@
-
+from back.app.schemas.cpi import CpiPeriod
+from back.app.services.cpi_parser import germany_historical_cpi_parser
 
 
 class CPIService:
-    pass
+    cpi_mapper = germany_historical_cpi_parser.cpi_data
+
+    def get_cpi(self, year: int, month: int) -> str | None:
+        """Получить CPI по году и месяцу."""
+        key = CpiPeriod(year=year, month=month)
+        return self.cpi_mapper.get(key)
