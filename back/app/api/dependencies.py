0ed7e2d98380eb156fdcbd753d440fa7b2e5e665
Artem Serdechnyi <artem.serdechnyy@gmail.com>
2026-02-03 16:33:49 +0200

    back: fixed dependencies

--- back/app/api/dependencies.py
+++ back/app/api/dependencies.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 from fastapi import Depends
 
 from back.app.services.ai_analysis_service import AIAnalysisService
@@ -8,14 +9,14 @@ from back.app.services.valuation_service import ValuationService
 def get_cpi_service() -> CPIService:
     return CPIService()
 
-cpi_service_dep = Depends(get_cpi_service)
+cpi_service_dep = Annotated[CPIService, Depends(get_cpi_service)]
 
 def get_valuation_service() -> ValuationService:
     return ValuationService()
 
-valuation_service_dep = Depends(get_valuation_service)
+valuation_service_dep = Annotated[ValuationService, Depends(get_valuation_service)]
 
 def get_ai_service() -> AIAnalysisService:
     return AIAnalysisService()
 
-ai_service_dep = Depends(get_ai_service)
+ai_service_dep = Annotated[AIAnalysisService, Depends(get_ai_service)]
