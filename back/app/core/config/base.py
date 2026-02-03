b2e35e39bb0872e72d0c5a1f6a5d1d313e7aea94
Artem Serdechnyi <artem.serdechnyy@gmail.com>
2026-02-03 15:02:37 +0200

    back: setup project

--- back/app/core/config/base.py
+++ back/app/core/config/base.py
@@ -1,7 +1,20 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+BASE_DIR = Path(__file__).resolve().parents[4]
+
 class _BaseConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True, extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        case_sensitive=True,
+        extra="ignore",
+    )
 
 class Settings(_BaseConfig):
+    SERVER_HOST: str
+    SERVER_PORT: int
+    RELOAD: bool = True
     FRONTEND_URLS: list[str]
+
+
+settings = Settings()
--- back/main.py
+++ back/main.py
@@ -1,10 +1,14 @@
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
+import uvicorn
 from loguru import logger
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from back.app.core.config import settings
+
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI) -> AsyncIterator[None]:
@@ -26,4 +30,20 @@ def _add_middleware(app: FastAPI) -> None:
         allow_headers=["*"],
     )
 
-settings = Settings()
+def create_app() -> FastAPI:
+    app = FastAPI(
+        lifespan=lifespan,
+        title="COA_API",
+    )
+
+    _include_router(app)
+    _add_middleware(app)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "back.main:create_app",
+        factory=True,
+        host=settings.SERVER_HOST,
+        port=settings.SERVER_PORT,
+        reload=settings.RELOAD,
+    )
